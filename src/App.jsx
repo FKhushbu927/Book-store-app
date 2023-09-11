14b20a761fac285d77b3f9bae5e45bbb3e949d16
FKhushbu927 <jannateekhushbu927@gmail.com>
2023-09-11 17:07:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+       <h1 className="text-3xl text-red-600 font-bold">Hellow world</h1>
+      
+    </>
+  )
+}
+
+export default App
