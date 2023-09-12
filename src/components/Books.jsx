2738f8ce4f637369534a369e212c8b2925fc2f95
FKhushbu927 <jannateekhushbu927@gmail.com>
2023-09-12 22:59:43 +0600

    data loaded to bokks pages from api

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Books = () => {
-    const booksData = useLoaderData();
-    console.log(booksData);
+    const {books} = useLoaderData();
+    
     return (
-        <div>
-           
+        <div className='my-container'>
+           <div className='grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2'>
+                {books.map(book => (
+                    <div>
+                              <img 
+                              src={book.image} 
+                              alt="book-cover" 
+                              className='object-cover w-full h-56 md:h-64 xl:h-80'/>
+                    </div>
+                ))}
+           </div>
         </div>
     );
 };
