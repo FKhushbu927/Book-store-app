82a6ddd69e605bd4d2bfa7ff48a39aa5238b7500
FKhushbu927 <jannateekhushbu927@gmail.com>
2023-09-12 23:40:55 +0600

    design added to book-cover

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -8,11 +8,21 @@ const Books = () => {
         <div className='my-container'>
            <div className='grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2'>
                 {books.map(book => (
-                    <div>
+                    <div className='overflow-hidden relative  transition duration-200 transform hover:-translate-y-2 rounded shadow-lg hover:shadow-2xl'>
                               <img 
                               src={book.image} 
                               alt="book-cover" 
-                              className='object-cover w-full h-56 md:h-64 xl:h-80'/>
+                              className='object-cover w-full h-56 md:h-64 xl:h-80'
+                              />
+
+                              <div className='bg-black px-6 py-4 bg-opacity-75  opacity-0 hover:opacity-100  inset-0 text-gray-300 transition-opacity duration-200 absolute flex flex-col'>
+                                <p>{book.title}</p>
+                                 <br />
+                                 <p>{book.subtitle.substring(0, 45)}...</p>
+                                 <br />
+                                 <h4 className='mt-auto font-bold'>Price:{book.price}</h4>
+
+                              </div>
                     </div>
                 ))}
            </div>
